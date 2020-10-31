8bff13b8fd39f8dc82408e61f63935a25ee02160
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-10-30 21:50:46 -0700

    Added : functionality to validate Email ID

--- userRegistration.java
+++ userRegistration.java
@@ -3,28 +3,42 @@ import java.util.Scanner;
 public class userRegistration {
     Scanner sc = new Scanner(System.in);
 
-
     public static void main(String[] args) {
-
         userRegistration obj = new userRegistration();
-        if (obj.checkName() == true) {
+        if (obj.checkName()  == true) {
             System.out.println("Entered name is in correct format");
-        } else{
+        } else {
             System.out.println("PLEASE Enter  a valid name");
         }
-
+        userRegistration obj1 = new userRegistration();
+        if(obj1.checkEmail() == true) {
+            System.out.println("Entered email is in correct format");
+        } else {
+            System.out.println("Enter valid email");
+        }
     }
+
     public boolean checkName() {
         System.out.println("Enter the  first name");
         String firstName = sc.next();
         System.out.println("Enter the last name");
         String lastName = sc.next();
 
-        if (firstName.matches("[A-Z][a-z]{3,}" ) && (lastName.matches("[A-Z][a-z]{3,}") == true) == true)  {
+        if (firstName.matches("[A-Z][a-z]{3,}") && (lastName.matches("[A-Z][a-z]{3,}") == true) == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkEmail() {
+        System.out.println("Enter the email");
+        String email = sc.next();
+        if(email.matches( "^[a-zA-Z]+([._+-][a-zA-Z]+)*[@][A-Za-z]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$" ) == true){
             return true;
         }
         else {
             return false;
         }
     }
-}
+  }
